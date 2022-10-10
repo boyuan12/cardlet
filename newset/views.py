91b8d5c2dffd8fb008f51cc236731b6a28ce0cbe
Boyuan Liu <boyuanliu6@yahoo.com>
2022-10-09 21:39:14 -0700

    fixed new set frontend

--- newset/views.py
+++ newset/views.py
@@ -1,10 +1,12 @@
-from ast import Pass
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt
 def new_set(request):
     if request.method == "POST":
-        pass
-
+        json_data = json.loads(request.body)
+        print(json_data)
     else:
         return render(request, "newset/new.html")
